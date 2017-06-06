2fb49f831e7ee60bd5bdb481fc2935a1caa38a84
Satoru Takahashi <fugasat@gmail.com>
2017-06-06 09:06:17 +0900

    メタデータ管理クラスのひな形を追加

---
+++ webapps/Features.py
@@ -0,0 +1,23 @@
+import os.path
+import pandas as pd
+
+
+class Features():
+    def __init__(self):
+        # self.__df = ???
+        pass
+
+    def get(self, uid, key):
+        return self.__df[uid]
+
+    def get_angle(self, uid):
+        return self.__df[uid]
+
+    def get_scene(self, uid):
+        return self.__df[uid]
+
+    def get_area(self, uid):
+        return self.__df[uid]
+
+    def get_model(self, uid):
+        return self.__df[uid]
