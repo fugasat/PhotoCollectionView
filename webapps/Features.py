c4fbf8c678fd174fa75b2bf6ce635f716b203a48
Satoru Takahashi <fugasat@gmail.com>
2017-06-09 09:11:37 +0900

    Featureテストコード追加

--- webapps/Features.py
+++ webapps/Features.py
@@ -17,11 +17,27 @@ class Features():
         row_dict = row_area.to_dict()
         return row_dict
 
+    def get_rail(self, uid):
+        row = self.df[self.df["ID"] == uid].iloc[0]
+        row_feature = row.loc["直線":"曲線"]
+        row_dict = row_feature.to_dict()
+        return row_dict
+
     def get_scene(self, uid):
-        return self.df[uid]
+        row = self.df[self.df["ID"] == uid].iloc[0]
+        row_feature = row.loc["森林":"踏切"]
+        row_no_feature = pd.Series({"scene特徴なし": row["scene特徴なし"]})
+        row_feature = row_feature.append(row_no_feature)
+        row_dict = row_feature.to_dict()
+        return row_dict
 
     def get_area(self, uid):
-        return self.df[uid]
+        row = self.df[self.df["ID"] == uid].iloc[0]
+        row_feature = row.loc["北海道":"九州"]
+        row_no_feature = pd.Series({"area特徴なし": row["area特徴なし"]})
+        row_feature = row_feature.append(row_no_feature)
+        row_dict = row_feature.to_dict()
+        return row_dict
 
     def get_model(self, uid):
         return self.df[uid]
--- webapps_test/test_features.py
+++ webapps_test/test_features.py
@@ -13,4 +13,22 @@ def test_get():
 
 
 def test_get_angle():
-    assert {"正面":0, "側面":0, "斜面":1, "高い":0, "遠い":0} == features.get_angle(1386)
+    assert {"正面": 0, "側面": 0, "斜面": 1, "高い": 0, "遠い": 0} == features.get_angle(1386)
+    assert {"正面": 1, "側面": 0, "斜面": 0, "高い": 1, "遠い": 1} == features.get_angle(1395)
+
+
+def test_get_rail():
+    assert {"直線": 0, "曲線": 1} == features.get_rail(1267)
+    assert {"直線": 1, "曲線": 0} == features.get_rail(1213)
+
+
+def test_get_scene():
+    assert {"森林": 0, "駅": 1, "畑": 0, "鉄橋": 0, "踏切": 0, "scene特徴なし": 0} == features.get_scene(1386)
+    assert {"森林": 0, "駅": 0, "畑": 0, "鉄橋": 0, "踏切": 0, "scene特徴なし": 1} == features.get_scene(1395)
+
+
+def test_get_area():
+    assert {"北海道": 1, "東北": 0, "東海": 0, "首都圏": 0, "中央": 0, "信越": 0, "北陸": 0, "九州": 0, "area特徴なし": 0}\
+           == features.get_area(1267)
+    assert {"北海道": 0, "東北": 0, "東海": 1, "首都圏": 1, "中央": 0, "信越": 1, "北陸": 0, "九州": 0, "area特徴なし": 0}\
+           == features.get_area(1384)
