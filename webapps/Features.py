5031354e4084081d300201996d0cbf8074b5290b
Satoru Takahashi <fugasat@gmail.com>
2017-06-13 12:47:11 +0900

    有効な値のlistを返す関数を追加

--- webapps/Features.py
+++ webapps/Features.py
@@ -13,15 +13,35 @@ class Features():
 
     def get_angle(self, uid):
         row = self.df[self.df["ID"] == uid].iloc[0]
-        row_area = row.loc["正面":"遠い"]
-        row_dict = row_area.to_dict()
+        row_feature = row.loc["正面":"遠い"]
+        row_dict = row_feature.to_dict()
         return row_dict
+
+    def get_angle_values(self, uid):
+        df_row = self.df[self.df["ID"] == uid]
+        df_row_feature = df_row.loc[:, "正面":"遠い"]
+        df_row_feature_on = df_row_feature[df_row_feature > 0].dropna(axis=1)  # 値が"1"の列だけ抽出
+        array_values = df_row_feature_on.columns.values
+        list_values = list(array_values)  # np.append()は遅いので使わないこと
+        if "高い" not in list_values:
+            list_values.append("低い")
+        if "遠い" not in list_values:
+            list_values.append("近い")
+        return list_values
 
     def get_rail(self, uid):
         row = self.df[self.df["ID"] == uid].iloc[0]
         row_feature = row.loc["直線":"曲線"]
         row_dict = row_feature.to_dict()
         return row_dict
+
+    def get_rail_values(self, uid):
+        df_row = self.df[self.df["ID"] == uid]
+        df_row_feature = df_row.loc[:, "直線":"曲線"]
+        df_row_feature_on = df_row_feature[df_row_feature > 0].dropna(axis=1)
+        array_values = df_row_feature_on.columns.values
+        list_values = list(array_values)
+        return list_values
 
     def get_scene(self, uid):
         row = self.df[self.df["ID"] == uid].iloc[0]
@@ -31,6 +51,14 @@ class Features():
         row_dict = row_feature.to_dict()
         return row_dict
 
+    def get_scene_values(self, uid):
+        df_row = self.df[self.df["ID"] == uid]
+        df_row_feature = df_row.loc[:, "森林":"踏切"]
+        df_row_feature_on = df_row_feature[df_row_feature > 0].dropna(axis=1)
+        array_values = df_row_feature_on.columns.values
+        list_values = list(array_values)
+        return list_values
+
     def get_area(self, uid):
         row = self.df[self.df["ID"] == uid].iloc[0]
         row_feature = row.loc["北海道":"九州"]
@@ -39,5 +67,25 @@ class Features():
         row_dict = row_feature.to_dict()
         return row_dict
 
+    def get_area_values(self, uid):
+        df_row = self.df[self.df["ID"] == uid]
+        df_row_feature = df_row.loc[:, "北海道":"九州"]
+        df_row_feature_on = df_row_feature[df_row_feature > 0].dropna(axis=1)
+        array_values = df_row_feature_on.columns.values
+        list_values = list(array_values)
+        return list_values
+
     def get_model(self, uid):
-        return self.df[uid]
+        row = self.df[self.df["ID"] == uid].iloc[0]
+        row_feature = row.loc["183":]
+        row_dict = row_feature.to_dict()
+        return row_dict
+
+    def get_model_values(self, uid):
+        df_row = self.df[self.df["ID"] == uid]
+        df_row_feature = df_row.loc[:, "183":]
+        df_row_feature_on = df_row_feature[df_row_feature > 0].dropna(axis=1)
+        array_values = df_row_feature_on.columns.values
+        list_values = list(array_values)
+        return list_values
+
--- webapps_test/test_features.py
+++ webapps_test/test_features.py
@@ -17,9 +17,19 @@ def test_get_angle():
     assert {"正面": 1, "側面": 0, "斜面": 0, "高い": 1, "遠い": 1} == features.get_angle(1395)
 
 
+def test_get_angle_values():
+    assert ["斜面", "低い", "近い"] == features.get_angle_values(1386)
+    assert ["正面", "高い", "遠い"] == features.get_angle_values(1395)
+
+
 def test_get_rail():
     assert {"直線": 0, "曲線": 1} == features.get_rail(1267)
     assert {"直線": 1, "曲線": 0} == features.get_rail(1213)
+
+
+def test_get_rail_values():
+    assert ["直線"] == features.get_rail_values(1213)
+    assert ["曲線"] == features.get_rail_values(1395)
 
 
 def test_get_scene():
@@ -27,8 +37,32 @@ def test_get_scene():
     assert {"森林": 0, "駅": 0, "畑": 0, "鉄橋": 0, "踏切": 0, "scene特徴なし": 1} == features.get_scene(1395)
 
 
+def test_get_scene_values():
+    assert ["駅", "鉄橋"] == features.get_scene_values(1384)
+    assert ["森林", "踏切"] == features.get_scene_values(1318)
+    assert [] == features.get_scene_values(1395)
+
+
 def test_get_area():
     assert {"北海道": 1, "東北": 0, "東海": 0, "首都圏": 0, "中央": 0, "信越": 0, "北陸": 0, "九州": 0, "area特徴なし": 0}\
            == features.get_area(1267)
     assert {"北海道": 0, "東北": 0, "東海": 1, "首都圏": 1, "中央": 0, "信越": 1, "北陸": 0, "九州": 0, "area特徴なし": 0}\
            == features.get_area(1384)
+
+
+def test_get_area_values():
+    assert ["東海", "首都圏", "信越"] == features.get_area_values(1384)
+    assert ["北海道", "東北", "中央", "信越", "北陸", "九州"] == features.get_area_values(1211)
+    assert [] == features.get_area_values(1404)
+
+
+def test_get_model():
+    assert {'dc72': 0, '24': 0, '24h': 0, '415': 0, 'old_pc': 1, 'ef58': 1, 'dc40h': 0, 'ef64': 0, 'ef62': 0, 'd51': 0, 'dc40': 0, '113y': 0, '113': 0, 'dc58': 0, 'c62': 0, '14': 0, 'ed75': 0, 'dc52': 0, 'dc281': 0, '10': 0, 'ef65pf': 0, 'df50': 0, '457': 0, 'dd51': 0, 'dd13': 0, 'ef65': 0, 'ed76': 0, 'ef63': 0, 'dc30': 0, 'nimotu': 0, '485': 0, 'de10': 0, '455': 0, 'dc91': 0, 'dc22': 0, 'fre': 0, 'ef66': 0, '157': 0, '14z': 0, 'ef15': 0, 'ef80': 0, 'ed78': 0, '183': 0, 'dc82': 0, '115': 0, 'ef64_10': 0, 'iida': 0, '153': 0, '583': 0, '165': 0, '20': 0, 'ef81': 0, '185': 0, 'nse': 0, '403': 0}\
+           == features.get_model(1384)
+    assert {'ef62': 0, 'c62': 0, '185': 0, '113': 0, 'ed76': 0, '14': 1, 'ef58': 0, 'nimotu': 1, '20': 0, 'dc82': 0, 'ef64': 0, '485': 0, 'dc91': 0, 'dc281': 0, '455': 0, 'dc22': 0, 'dc58': 0, 'ef64_10': 0, 'fre': 0, '24': 0, 'dc52': 0, '14z': 0, 'ef65pf': 0, '415': 0, '10': 0, '183': 0, 'ef81': 0, 'de10': 0, 'ef65': 0, 'df50': 0, 'ed75': 0, '153': 0, 'ef80': 0, 'ef66': 0, '457': 0, 'ef63': 0, '113y': 0, 'ef15': 0, '583': 0, '165': 0, 'dc40h': 0, 'iida': 0, '24h': 0, '115': 0, 'dc40': 0, 'd51': 0, 'ed78': 0, 'dc30': 0, '403': 0, 'nse': 0, '157': 0, 'dc72': 0, 'dd13': 0, 'old_pc': 0, 'dd51': 1}\
+           == features.get_model(1267)
+
+
+def test_get_model_values():
+    assert ["14", "dd51", "nimotu"] == features.get_model_values(1267)
+    assert ["ef58", "old_pc"] == features.get_model_values(1384)
