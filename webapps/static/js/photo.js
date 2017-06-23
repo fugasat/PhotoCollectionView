6721cc60885428995477f9876f4fca5b6296a294
Satoru Takahashi <fugasat@gmail.com>
2017-06-23 09:18:50 +0900

    modalに選択した写真を表示

--- webapps/static/js/photo.js
+++ webapps/static/js/photo.js
@@ -1,20 +1,23 @@
 $(function(){
     $(".a_photo").click(function(){
         var file_path = $(this).data('file-path');
-        create_table(file_path);
+        var img_url = '"/static/photos/' + file_path + '"'
+
+        $('#modal_body_photo').css('background-image', 'url(' + img_url + ')');
+        create_table("#modal_body_photo_relation", file_path);
 
         $('#modal_thumbnail').modal('show');
     });
 });
 
-function create_table(file_path) {
-    $("#modal_body_thumbnail").empty();
-    $("#modal_body_thumbnail").append("<h5 class='text-center'>Hello. Some text here.</h5>");
-    $("#modal_body_thumbnail").append(get_table_body(file_path));
-    $("#modal_body_thumbnail").append(get_table_body("1001.jpg"));
-    $("#modal_body_thumbnail").append(get_table_body("1002.jpg"));
-    $("#modal_body_thumbnail").append(get_table_body("1003.jpg"));
-    $("#modal_body_thumbnail").append(get_table_body("1004.jpg"));
+function create_table(parent_id, file_path) {
+    $(parent_id).empty();
+    $(parent_id).append("<h5 class='text-center'>Hello. Some text here.</h5>");
+    $(parent_id).append(get_table_body("1000.jpg"));
+    $(parent_id).append(get_table_body("1001.jpg"));
+    $(parent_id).append(get_table_body("1002.jpg"));
+    $(parent_id).append(get_table_body("1003.jpg"));
+    $(parent_id).append(get_table_body("1004.jpg"));
 }
 
 function get_table_body(file_path) {
