2a7164603a11ee65ab65b419f0865b2eb613fcdc
Satoru Takahashi <fugasat@gmail.com>
2017-06-27 09:06:46 +0900

    modalレイアウトを調整（関連画像をmain/subに分けて表示）

--- webapps/static/js/photo.js
+++ webapps/static/js/photo.js
@@ -4,23 +4,43 @@ $(function(){
         var img_url = '"/static/photos/' + file_path + '"'
 
         $('#modal_body_photo').css('background-image', 'url(' + img_url + ')');
-        create_table("#modal_body_photo_relation", file_path);
+
+        create_main_relation_table("#modal_body_photo_main_relation");
+        create_sub_relation_table("#modal_body_photo_sub_relation", file_path);
 
         $('#modal_thumbnail').modal('show');
+
+        return false;
     });
 });
 
-function create_table(parent_id, file_path) {
+//
+// Main relation
+//
+function create_main_relation_table(parent_id) {
     $(parent_id).empty();
-    $(parent_id).append("<h5 class='text-center'>Hello. Some text here.</h5>");
-    $(parent_id).append(get_table_body("1000.jpg"));
-    $(parent_id).append(get_table_body("1001.jpg"));
-    $(parent_id).append(get_table_body("1002.jpg"));
-    $(parent_id).append(get_table_body("1003.jpg"));
-    $(parent_id).append(get_table_body("1004.jpg"));
+    for (var i = 0; i < 6; i++) {
+        var file_path = "1000.jpg";
+        var img_url = '"/static/photos/' + file_path + '"';
+        var item = "<div class='col-sm-4 modal_main_thumbnail_row'><div class='thumbnail' style='background-image: url(" + img_url + ")'></div></div>";
+        $(parent_id).append(item);
+    }
 }
 
-function get_table_body(file_path) {
+//
+// Sub relation
+//
+function create_sub_relation_table(parent_id, file_path) {
+    $(parent_id).empty();
+    $(parent_id).append("<h5 class='text-center'>Hello. Some text here.</h5>");
+    $(parent_id).append(get_sub_relation_table_body("1000.jpg"));
+    $(parent_id).append(get_sub_relation_table_body("1001.jpg"));
+    $(parent_id).append(get_sub_relation_table_body("1002.jpg"));
+    $(parent_id).append(get_sub_relation_table_body("1003.jpg"));
+    $(parent_id).append(get_sub_relation_table_body("1004.jpg"));
+}
+
+function get_sub_relation_table_body(file_path) {
     var table_header =
         "<div class='table-responsive'>" +
             "<table class='table table-striped modal_thumbnail_table'>" +
@@ -31,11 +51,11 @@ function get_table_body(file_path) {
                 "</tbody>" +
             "</table>" +
         "</div>";
-    var table_data = get_table_data(file_path)
+    var table_data = get_sub_relation_table_data(file_path)
     return table_header + table_data + table_footer;
 }
 
-function get_table_data(file_path) {
+function get_sub_relation_table_data(file_path) {
     var table_data = ""
     for (var i = 0; i < 10; i++) {
         var img_url = '"/static/photos/' + file_path + '"'
